a4e2bcc603e1ddd4c80a34dc0c6312706f697e41
electricg <electric.g@gmail.com>
2016-07-07 17:23:27 +0100

    Started

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,60 @@
 /* jshint esnext: true */
+var CACHE_NAME = 'v1';
+var urlsToCache = [
+  '/',
+  '/workshops',
+  '/css/style.css',
+  '/js/script.js',
+  '/images/left-logic.svg'
+];
 
-// It's too early to implement offline support or other niceties
-
-// https://remysharp.com/2016/03/22/the-copy--paste-guide-to-your-first-service-worker
-const cacheName = 'v1::static';
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        '/',
-      ]).then(() => self.skipWaiting());
-    })
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have 2 stream.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
