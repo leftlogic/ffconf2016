38c6a9483c51b2651ef5ff2b17255d61258fc623
electricg <electric.g@gmail.com>
2016-07-20 22:55:12 +0100

    Removed push notifications

--- public/sw.js
+++ public/sw.js
@@ -1,71 +1,22 @@
-/* global self, clients */
 /* jshint esnext: true */
-var TITLE = 'Notification from ffconf 2016';
-var OPEN_URL = 'https://2016.ffconf.org/';
-var BODY = 'Default push message';
-var ICON = 'images/favicons/notifications.png';
 
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-  console.log('Installed', event);
-});
+// It's too early to implement offline support or other niceties
 
-self.addEventListener('activate', function(event) {
-  console.log('Activated', event);
-});
+// https://remysharp.com/2016/03/22/the-copy--paste-guide-to-your-first-service-worker
+const cacheName = 'v1::static';
 
-self.addEventListener('push', function(event) {
-  console.log('Received push');
-  var notificationTitle = TITLE;
-  var notificationOptions = {
-    body: BODY,
-    icon: ICON
-  };
-
-  if (event.data) {
-    var msg = {
-      t: TITLE,
-      b: BODY
-    };
-    try {
-      var tmp = JSON.parse(event.data.text());
-      msg.t = tmp.t || msg.t;
-      msg.b = tmp.b || msg.b;
-    } catch(e) {}
-    notificationTitle = msg.t;
-    notificationOptions.body = msg.b;
-  }
-
-  event.waitUntil(
-    self.registration.showNotification(notificationTitle, notificationOptions)
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+      ]).then(() => self.skipWaiting());
+    })
   );
 });
 
-self.addEventListener('notificationclick', function(event) {
-  console.log('Notification click', event.notification);
-  // Android doesn't close the notification when you click it
-  // See http://crbug.com/463146
-  event.notification.close();
-  var url = OPEN_URL;
-  // Check if there's already a tab open with this URL.
-  // If yes: focus on the tab.
-  // If no: open a tab with the URL.
-  event.waitUntil(
-    clients.matchAll({
-      type: 'window'
-    })
-    .then(function(windowClients) {
-      console.log('WindowClients', windowClients);
-      for (var i = 0; i < windowClients.length; i++) {
-        var client = windowClients[i];
-        console.log('WindowClient', client);
-        if (client.url === url && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow(url);
-      }
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
