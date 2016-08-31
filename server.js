2f6d03f7bf119394a5d0c8e6f92d5cea6c490f94
Remy Sharp <remy@remysharp.com>
2016-08-31 15:11:46 +0100

    feat: make package requireable

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var harp = require('harp');
-var server = require('harp-static');
+// var server = require('harp-static');
 var moment = require('moment');
 var outputPath = __dirname + '/www';
 var port = process.env.PORT || 9000;
+var express = require('express');
+var app = express();
 
 global.idify = s => {
   return s.replace(/&.*?;/g, '')
@@ -19,28 +21,40 @@ global.moment = moment;
 
 if (process.env.NODE_ENV !== 'production') {
   // Yeah...don't ask.
-  var config = require('./harp.json');
-  Object.keys(config.globals).forEach(function (key) {
-    global[key] = config.globals[key];
-  });
+  // var config = require('./harp.json');
+  // Object.keys(config.globals).forEach(function (key) {
+  //   global[key] = config.globals[key];
+  // });
   // </rando>
-  var express = require('express');
-  var app = express();
-
   app.use(express.static(__dirname + '/public'));
   app.use(harp.mount(__dirname + '/public'));
 
-  var server = app.listen(port, function(){
-    console.log('Listening at http://%s:%s', server.address().address, server.address().port);
-  });
+  if (module.parent) {
+    module.exports = app;
+  } else {
+    var server = app.listen(port, function(){
+      console.log('Listening at http://%s:%s', server.address().address, server.address().port);
+    });
+  }
+
 } else {
-  harp.compile(__dirname, outputPath, (errors) => {
+  harp.compile(__dirname + '/public', outputPath, (errors) => {
     if (errors) {
       console.log(JSON.stringify(errors, null, 2));
       process.exit(1);
     }
 
-    console.log('Running harp-static on ' + port);
-    server(outputPath, port);
+    // server(outputPath, port);
+    app.use(express.static(__dirname + '/www', { extensions: ['html'] }));
+    if (module.parent) {
+      module.exports = app;
+    } else {
+      console.log('Running harp-static on ' + port);
+      var server = app.listen(port, function(){
+        console.log('Listening at http://%s:%s', server.address().address, server.address().port);
+      });
+    }
+
+
   });
 }
